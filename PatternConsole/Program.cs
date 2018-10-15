f17f9640559f503424b0fc0880c2f141f3775ae7
aski_vn <aski_vn@mail.ru>
2018-10-15 19:02:16 +0300

    Formal adapter github

---
+++ PatternConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using PatternsGoF.Adapter;
+
+
+namespace PatternConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            program.AdapterFormal();
+        }
+
+        private void AdapterFormal()
+        {
+            AdapterFormal adapterFormal = new AdapterFormal();
+            adapterFormal.Main();
+            Console.ReadKey();
+        } 
+    }
+}
---
+++ PatternsGoF/Adapter/AdapterFormal.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternsGoF.Adapter
+{
+    /// <summary>
+    /// MainApp startup class for Structural
+    /// Adapter Design Pattern.
+    /// </summary>
+    public class AdapterFormal
+    {
+        public void Main()
+        {
+            Target target = new Adapter();
+            target.Request();
+        }
+    }
+
+    /// <summary>
+    /// The 'Target' class
+    /// </summary>
+    class Target
+    {
+        public virtual void Request()
+    
+        {
+            Console.WriteLine("Called Target Request()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Adapter' class
+    /// </summary>
+    class Adapter : Target
+    {
+        private Adaptee _adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            // Possibly do some other work
+            //  and then call SpecificRequest
+            _adaptee.SpecificRequest();
+        }
+    }
+
+    /// <summary>
+    /// The 'Adaptee' class
+    /// </summary>
+    class Adaptee
+    {
+        public void SpecificRequest()
+        {
+            Console.WriteLine("Called SpecificRequest()");
+        }
+    }
+}
